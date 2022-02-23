13b4c26e07f077685650576d106595e214e57880
LiJell <hanjukim0922@gmail.com>
2022-02-23 14:04:57 +0900

    preprocessed_total_data_per_area_edit_re

--- preprocessed_total_data_per_area.py
+++ preprocessed_total_data_per_area.py
@@ -8,9 +8,9 @@ import pandas as pd
 import numpy as np
 # 2020
 total = pd.read_excel('D:/multi_project_sample_data_/data_total_2020.xlsx')
-total
+total_a = total.copy()
 
-total_1 = total[total.columns.difference(['반려동물 가구 비율(%, 인구 수)', '행복지수 종합'])]
+total_1 = total_a[total_a.columns.difference(['반려동물 가구 비율(%, 인구 수)', '행복지수 종합'])]
 total_1
 
 
@@ -22,13 +22,14 @@ total_2.columns
 len(total.columns)
 for i in range(len(total_2.T)):
     name = total_2.columns[i] + '_per_area'
-    total[name] = total_2.iloc[:,i]/total['면적(㎢)']    
+    total_a[name] = total_2.iloc[:,i]/total['면적(㎢)']    
  
 
-total = total.iloc[:,-13:-1]
+total_b = total_a.iloc[:,-11:]
+total_place = total_a['자치구']
+total_final = pd.concat([total_b,total_place], axis=1)
+total_final.to_excel('D:/multi_project_sample_data_/2020_total_per_area.xlsx', index = True)
 
-total.to_excel('D:/multi_project_sample_data_/2020_total_per_area.xlsx', index = False)
 
-total.info()
 
 
